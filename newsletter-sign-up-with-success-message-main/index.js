d7d15c6e54e6dbc30fbea7ba1a8672a5a65b33d2
1skar05 <liamlee7797@gmail.com>
2023-09-18 23:16:39 +0700

    add email validate

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -1,10 +1,25 @@
 let btnSubscribe = document.getElementById("subscribe");
 let btnDismiss = document.getElementById("dismiss");
 let modal = document.getElementById("modal");
+let formWrapper = document.getElementById("form-wrapper");
+let error = document.getElementById("error");
+
+const emailRegex =
+  /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
 btnSubscribe.addEventListener("click", function () {
-  modal.classList.add("modal-open");
+  let emailInput = document.getElementById("email");
+  let email = emailInput.value;
+  if (email.match(emailRegex)) {
+    modal.classList.add("modal-open");
+    formWrapper.style.display = "none";
+  } else {
+    error.style.display = "unset";
+    emailInput.focus();
+  }
 });
 
 btnDismiss.addEventListener("click", function () {
   modal.classList.remove("modal-open");
+  formWrapper.style.display = "unset";
 });
